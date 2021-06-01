a6440d0148fdc8eae8ec074ed36e969afc438741
Davi <48662592+d-mageek@users.noreply.github.com>
2021-05-31 21:55:18 -0300

    stuff

---
+++ Client/Functions/Triggers.cs
@@ -0,0 +1,82 @@
+﻿using Client.Functions.Utils;
+using System;
+using MelonLoader;
+using UIExpansionKit.API;
+using VRC.SDKBase;
+using UnityEngine;
+
+namespace Client.Functions
+{
+    internal static class Triggers
+    {
+        private static bool IsForceGlobal = false;
+        public static bool IsOn;
+        public static bool IsAlwaysForceGlobal = false;
+
+        public static void OnApplicationStart()
+        {
+            MelonPreferences.CreateCategory("LocalToGlobal", "PM - Local To Global");
+            MelonPreferences.CreateEntry("LocalToGlobal", "IsOn", false, "Activate Mod? This is a risky function.");
+        }
+
+        public static void OnPreferencesSaved()
+        {
+            IsOn = MelonPreferences.GetEntryValue<bool>("LocalToGlobal", "IsOn");
+        }
+
+        public static void ShowTriggersMenu()
+        {
+            ICustomShowableLayoutedMenu TriggersMenu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu3Columns);
+            TriggersMenu.AddSimpleButton("Go back", () => Main.ClientMenu.Show());
+            string btnName = "LocalToGlobal - ";
+            Action action = null;
+            if (IsOn && Utils.Utilities.IsSDK2World())
+            {
+                btnName += "On";
+                action = () => ShowLocalToGlobalMenu();
+            }
+            else if (!IsOn)
+            {
+                btnName += "Off";
+                action = () => Main.RiskyFuncAlert("LocalToGlobal");
+            }
+            else
+            {
+                btnName += "Off";
+                action = () => Methods.PopupV2(
+                    "LocalToGlobal",
+                    "Sorry, this world is an SDK3 Udon world.\nThis function won't work in here.",
+                    "Close",
+                    new Action(() => { VRCUiManager.prop_VRCUiManager_0.HideScreen("POPUP"); }));
+            }
+            TriggersMenu.AddSimpleButton(btnName, action);
+            foreach (VRC_Trigger trigger in Resources.FindObjectsOfTypeAll<VRC_Trigger>()) TriggersMenu.AddSimpleButton(trigger.name, () => TriggerMenu(trigger));
+            TriggersMenu.Show();
+        }
+
+        private static void ShowLocalToGlobalMenu()
+        {
+            ICustomShowableLayoutedMenu LocalToGlobalMenu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu3Columns);
+            LocalToGlobalMenu.AddSimpleButton("Go back", () => ShowTriggersMenu());
+            LocalToGlobalMenu.AddToggleButton("Always Force Local to Global", (_) => IsAlwaysForceGlobal = !IsAlwaysForceGlobal, () => IsAlwaysForceGlobal);
+            LocalToGlobalMenu.AddToggleButton("Force Local to Global on Trigger", (_) => IsForceGlobal = !IsForceGlobal, () => IsForceGlobal);
+            LocalToGlobalMenu.Show();
+        }
+
+        private static void TriggerMenu(VRC_Trigger trigger)
+        {
+            ICustomShowableLayoutedMenu TriggerMenu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu4Columns);
+            TriggerMenu.AddSimpleButton("Go back", () => ShowTriggersMenu());
+            foreach (VRC_Trigger.TriggerEvent @event in trigger.Triggers) TriggerMenu.AddSimpleButton(@event.Name, () => 
+            { 
+                try 
+                { 
+                    if (IsForceGlobal)
+                        NativePatches.triggerOnce = true; 
+                    trigger.ExecuteTrigger.Invoke(@event); 
+                } catch { } 
+            });
+            TriggerMenu.Show();
+        }
+    }
+}
