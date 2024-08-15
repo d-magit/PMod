69a875680052b9282a68561d72dc2d440adc12f4
Elaina <48662592+oestradiol@users.noreply.github.com>
2024-08-15 17:41:27 -0300

    Beep

--- PMod/Main.cs
+++ PMod/Main.cs
@@ -1,4 +1,5 @@
 ﻿using PMod.Utils;
+using PMod.Loader;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -6,7 +7,7 @@ using UnhollowerRuntimeLib;
 using MelonLoader;
 using UIExpansionKit.API;
 using VRC;
-using PMod.Loader;
+using VRC.SDKBase;
 
 [assembly: AssemblyTitle(PMod.BuildInfo.Name)]
 [assembly: AssemblyCopyright("Created by " + PMod.BuildInfo.Author)]
@@ -80,6 +81,7 @@ namespace PMod
         {
             ClientMenu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu3Columns);
             ClientMenu.AddSimpleButton("Close Menu", ClientMenu.Hide);
+            ClientMenu.AddSimpleButton("Beep", () => SendRPC("GetBeepedLol"));
             ClientMenu.AddSimpleButton("Orbit", () =>
             {
                 if (ModulesManager.orbit.IsOn.Value) ModulesManager.orbit.OrbitMenu.Show();
@@ -97,6 +99,29 @@ namespace PMod
             });
             ClientMenu.AddSimpleButton("Triggers", () => ModulesManager.triggers.ShowTriggersMenu());
             ClientMenu.Show();
+        }
+
+        // Got from KiraiLib https://github.com/xKiraiChan/KiraiLibs/blob/9ccba43ca646860ec87f07ae364b81a87f568f5d/KiraiRPC/SendRPC.cs#L28
+        // Be careful using this since I literally didn't add check for Moderators lol
+        public static void SendRPC(string raw)
+        {
+            PLogger.Msg("Sending RPC...");
+            var handler = UnityEngine.Object.FindObjectOfType<VRC_EventHandler>();
+            handler.TriggerEvent(
+                new VRC_EventHandler.VrcEvent
+                {
+                    EventType = VRC_EventHandler.VrcEventType.SendRPC,
+                    Name = "SendRPC",
+                    ParameterObject = handler.gameObject,
+                    ParameterInt = Player.prop_Player_0.field_Private_VRCPlayerApi_0.playerId,
+                    ParameterFloat = 0f,
+                    ParameterString = "UdonSyncRunProgramAsRPC",
+                    ParameterBoolOp = VRC_EventHandler.VrcBooleanOp.Unused,
+                    ParameterBytes = Networking.EncodeParameters(new Il2CppSystem.Object[] {raw})
+                },
+                VRC_EventHandler.VrcBroadcastType.AlwaysUnbuffered, 
+                VRCPlayer.field_Internal_Static_VRCPlayer_0.gameObject, 
+                0f);
         }
     }
 }
